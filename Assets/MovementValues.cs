aedadc80956017f93131d7b255a3e64c421df46b
LauraOzolina <laura.ozolina15@va.lv>
2023-02-06 22:13:29 +0200

    base rotate,buttons

---
+++ Assets/MovementValues.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementValues : MonoBehaviour
+{
+    public float height,longitudinal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        height = 0f;
+        longitudinal = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/TouchKnob.cs
+++ Assets/TouchKnob.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.UI;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class TouchKnob : MonoBehaviour
 {
@@ -10,19 +13,27 @@ public class TouchKnob : MonoBehaviour
     float prevSpeed;
     int animationState;
     public GameObject raiseable;
-    public GameObject bedpush,movable,scanner;
+    public GameObject bedpush,movable,scanner,bedbase;
     AnimatorClipInfo[] m_CurrentClipInfo;
-    private bool is_Raising,is_Lowering,is_Scanning, is_Moving_out, is_Moving_in = false;
+    private bool is_Raising,is_Lowering,is_Scanning, is_Moving_out, is_Moving_in, is_Bed_Rotate_Left, is_Bed_Rotate_Right, is_Laser_Off = false;
     private int direction;
+    public MovementValues mvalues;
+    public GameObject heighttmp;
+    public GameObject longitudinaltmp;
     void Start()
     {
-         anim = elekta.GetComponent<Animator>();
-         prevSpeed = anim.speed;
-         raiseable = GameObject.FindWithTag("raiseable");
+
+        anim = elekta.GetComponent<Animator>();
+        prevSpeed = anim.speed;
+        raiseable = GameObject.FindWithTag("raiseable");
         bedpush = GameObject.FindWithTag("bedpush");
         movable = GameObject.FindWithTag("movable");
         scanner = GameObject.FindWithTag("scanner");
+        bedbase = GameObject.FindWithTag("bedbaserot");
+        heighttmp = GameObject.FindWithTag("heighttmp");
+        longitudinaltmp = GameObject.FindWithTag("longitudinaltmp");
         direction = 2;
+     
     }
     void Update()
     {
@@ -31,27 +42,33 @@ public class TouchKnob : MonoBehaviour
             Debug.Log("its raising");
   
             Vector3 prevpos = raiseable.transform.localPosition;
-            if(raiseable.transform.localPosition.y <= 0.226f)
+            if(raiseable.transform.localPosition.y < 0.66f)
             {
                 raiseable.transform.localPosition = new Vector3(prevpos.x, (prevpos.y + 0.001f), prevpos.z);
                 Vector3 prevsize = bedpush.transform.localScale;
-                bedpush.transform.localScale = new Vector3(prevsize.x, (prevsize.y + 0.0024f), prevsize.z);
+                bedpush.transform.localScale = new Vector3(prevsize.x, (prevsize.y + 0.0033f), prevsize.z);
                 float dif = raiseable.transform.localPosition.y - bedpush.transform.localScale.y;
-                Debug.Log("dif" + dif);
+                mvalues.height = mvalues.height + 0.001f;
+                heighttmp.GetComponent<TMP_Text>().text = "Height: " + System.Math.Round(mvalues.height, 2);
+
+
+
             }
 
-          
+
         }
         if (is_Lowering)
         {
             Debug.Log("its lowering");
 
             Vector3 prevpos = raiseable.transform.localPosition;
-            if (raiseable.transform.localPosition.y >= 0.099f)
+            if (raiseable.transform.localPosition.y > 0.452f)
             {
                 raiseable.transform.localPosition = new Vector3(prevpos.x, (prevpos.y - 0.001f), prevpos.z);
                 Vector3 prevsize = bedpush.transform.localScale;
-                bedpush.transform.localScale = new Vector3(prevsize.x, (prevsize.y - 0.0024f), prevsize.z);
+                bedpush.transform.localScale = new Vector3(prevsize.x, (prevsize.y - 0.0033f), prevsize.z);
+                mvalues.height = mvalues.height - 0.001f;
+                heighttmp.GetComponent<TMP_Text>().text = "Height: " + System.Math.Round(mvalues.height,2);
             }
    
 
@@ -62,7 +79,8 @@ public class TouchKnob : MonoBehaviour
 
             Vector3 prevpos = movable.transform.localPosition;
             movable.transform.localPosition = new Vector3((prevpos.x + 0.001f), prevpos.y, prevpos.z);
-
+            mvalues.longitudinal = mvalues.longitudinal + 0.001f;
+            longitudinaltmp.GetComponent<TMP_Text>().text = "Longitudinal: " + System.Math.Round(mvalues.longitudinal, 2);
         }
         if (is_Moving_out)
         {
@@ -70,7 +88,25 @@ public class TouchKnob : MonoBehaviour
 
             Vector3 prevpos = movable.transform.localPosition;
             movable.transform.localPosition = new Vector3((prevpos.x - 0.001f), prevpos.y, prevpos.z);
+            mvalues.longitudinal = mvalues.longitudinal - 0.001f;
+            longitudinaltmp.GetComponent<TMP_Text>().text = "Longitudinal: " + System.Math.Round(mvalues.longitudinal, 2);
+        }
+        if (is_Bed_Rotate_Left)
+        {
+          
+                Vector3 rotationVector = bedbase.transform.eulerAngles;
+                Debug.Log(rotationVector);
+                bedbase.transform.eulerAngles = new Vector3(rotationVector.x, (rotationVector.y + 0.1f), rotationVector.z);
+            
+        }
 
+        if (is_Bed_Rotate_Right)
+        {
+           
+                Vector3 rotationVector = bedbase.transform.eulerAngles;
+                Debug.Log(rotationVector);
+                bedbase.transform.eulerAngles = new Vector3(rotationVector.x, (rotationVector.y - 0.1f), rotationVector.z);
+            
         }
 
     }
@@ -136,6 +172,71 @@ public class TouchKnob : MonoBehaviour
         is_Moving_in = false;
         //anim.speed = 0;
         Debug.Log("stopmoving");
+    }
+
+    public void Rotate_Bed_Left()
+    {
+        is_Bed_Rotate_Left = true;
+
+        Debug.Log("bed_rotate");
+
+    }
+    public void Rotate_Bed_Right()
+    {
+        is_Bed_Rotate_Right = true;
+
+        Debug.Log("bed_rotate");
+
+    }
+
+    public void Stop_Rotate_Bed()
+    {
+        is_Bed_Rotate_Left = false;
+        is_Bed_Rotate_Right = false;
+
+        Debug.Log("stop_bed_rotate");
+
+    }
+
+    public void Toggle_Laser()
+    {
+        GameObject[] lasers = GameObject.FindGameObjectsWithTag("laser");
+        Debug.Log(is_Laser_Off);
+        if (is_Laser_Off == true)
+        {
+            foreach (GameObject la in lasers)
+            {
+                Debug.Log(la);
+       
+                la.GetComponent<DecalProjector>().enabled = true;
+            }
+            is_Laser_Off = false;
+        }
+        else
+        {
+            foreach (GameObject la in lasers)
+            {
+                Debug.Log(la);
+                la.GetComponent<DecalProjector>().enabled = false;
+            }
+            is_Laser_Off = true;
+            
+        }
+  
+        Debug.Log("laser");
+
+    }
+
+    public void Toggle_Movable()
+    {
+        if (elekta.GetComponent<ObjectManipulator>().enabled)
+        {
+            elekta.GetComponent<ObjectManipulator>().enabled = false;
+        }
+        else
+        {
+            elekta.GetComponent<ObjectManipulator>().enabled = true;
+        }
     }
 
 }
