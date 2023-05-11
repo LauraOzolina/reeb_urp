dd1dddd5b62d6eeee35a92bb80f7cf330ffda203
LauraOzolina <laura.ozolina15@va.lv>
2023-05-11 23:14:10 +0300

    reeb ui slider buttons

---
+++ Assets/WhiteboardButtons.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhiteboardButtons : MonoBehaviour
+{
+    public GameObject taskview;
+    public GameObject settingsview;
+    public void Task1()
+    {
+        Debug.Log("task1");
+    }
+    public void Task2()
+    {
+        Debug.Log("task2");
+    }
+    public void Task3()
+    {
+        Debug.Log("task3");
+    }
+
+    public void Settings()
+    {
+        taskview.SetActive(false);
+        settingsview.SetActive(true);
+    }
+
+    public void Back()
+    {
+        taskview.SetActive(true);
+        settingsview.SetActive(false);
+    }
+}
