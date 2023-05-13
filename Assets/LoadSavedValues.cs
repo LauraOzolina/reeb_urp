fda121d932200a05044737a229d0fc2e3da1d18b
LauraOzolina <laura.ozolina15@va.lv>
2023-05-13 18:37:33 +0300

    task button colors treatment text

--- Assets/LoadSavedValues.cs
+++ Assets/LoadSavedValues.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoadSavedValues : MonoBehaviour
 {
@@ -23,12 +24,25 @@ public class LoadSavedValues : MonoBehaviour
     public GameObject t3_pat_x;
 
     public GameObject t3_pat_z;
+
+    public int activetask;
+    public GameObject btn1;
+    public GameObject btn2;
+    public GameObject btn3;
+    public GameObject treatmentcard;
+    public string txt;
     // Start is called before the first frame update
     void Start()
     {
+        activetask = 1;
+        btn1.GetComponent<Image>().color = new Color32(93, 195, 138, 255);
         t1_treat_x.GetComponent<TMP_Text>().text = PlayerPrefs.GetInt("t1_treatment_x", 0).ToString();
         t1_treat_y.GetComponent<TMP_Text>().text = PlayerPrefs.GetInt("t1_treatment_y", 0).ToString();
         t1_treat_z.GetComponent<TMP_Text>().text = PlayerPrefs.GetInt("t1_treatment_z", 0).ToString();
+
+        txt = "Treatment (left limb) :  X: " + t1_treat_x.GetComponent<TMP_Text>().text + " Y: " 
+        + t1_treat_y.GetComponent<TMP_Text>().text + " Z: " + t1_treat_z.GetComponent<TMP_Text>().text;
+        treatmentcard.GetComponent<TMP_Text>().text = txt;
 
         t2_treat_x.GetComponent<TMP_Text>().text = PlayerPrefs.GetInt("t2_treatment_x", 0).ToString();
         t2_treat_y.GetComponent<TMP_Text>().text = PlayerPrefs.GetInt("t2_treatment_y", 0).ToString();
--- Assets/WhiteboardButtons.cs
+++ Assets/WhiteboardButtons.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WhiteboardButtons : MonoBehaviour
 {
     public GameObject taskview;
     public GameObject settingsview;
+    public GameObject btn1;
+    public GameObject btn2;
+    public GameObject btn3;
+    public GameObject treatmentcard;
+    public string txt;
     void Start()
     {
         settingsview.SetActive(false);
@@ -13,14 +20,36 @@ public class WhiteboardButtons : MonoBehaviour
     public void Task1()
     {
         Debug.Log("task1");
+        btn1.GetComponent<Image>().color = new Color32(93, 195, 138, 255);
+        btn2.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+        btn3.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+
+
+        txt = "Treatment (left limb) :  X: " + PlayerPrefs.GetInt("t1_treatment_x", 0).ToString() + " Y: "
+        + PlayerPrefs.GetInt("t1_treatment_y", 0).ToString() + " Z: " + PlayerPrefs.GetInt("t1_treatment_z", 0).ToString();
+        treatmentcard.GetComponent<TMP_Text>().text = txt;
     }
     public void Task2()
     {
         Debug.Log("task2");
+        btn1.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+        btn2.GetComponent<Image>().color = new Color32(93, 195, 138, 255);
+        btn3.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+
+        txt = "Treatment (left limb) :  X: " + PlayerPrefs.GetInt("t2_treatment_x", 0).ToString() + " Y: "
+        + PlayerPrefs.GetInt("t2_treatment_y", 0).ToString() + " Z: " + PlayerPrefs.GetInt("t2_treatment_z", 0).ToString();
+        treatmentcard.GetComponent<TMP_Text>().text = txt;
     }
     public void Task3()
     {
         Debug.Log("task3");
+        btn1.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+        btn2.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+        btn3.GetComponent<Image>().color = new Color32(93, 195, 138, 255);
+
+        txt = "Treatment (left limb) :  X: " + PlayerPrefs.GetInt("t3_treatment_x", 0).ToString() + " Y: "
+        + PlayerPrefs.GetInt("t3_treatment_y", 0).ToString() + " Z: " + PlayerPrefs.GetInt("t3_treatment_z", 0).ToString();
+        treatmentcard.GetComponent<TMP_Text>().text = txt;
     }
 
     public void Settings()
